05e1af166e2dd2cb1730431bb24158b8f8a811eb
zlobasss <sergey4ik@outlook.com>
2024-08-22 18:18:50 +0700

    Added repositories

---
+++ src/main/java/com/example/promo/repository/PromoCodeRepository.java
@@ -0,0 +1,12 @@
+package com.example.promo.repository;
+
+import com.example.promo.entity.PromoCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PromoCodeRepository extends JpaRepository<PromoCode, Long> {
+    Optional<PromoCode> findByCode(String code);
+}
